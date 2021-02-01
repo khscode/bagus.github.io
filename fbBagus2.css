9611fc6ad6e0c4b47d4d9c4edd9d93b58254173f
khscode <55636836+khscode@users.noreply.github.com>
2021-02-01 11:51:36 +0700

    Create fbBagus2.css

---
+++ fbBagus2.css
@@ -0,0 +1,54 @@
+body {
+    background-color: #4a65a5 !important;
+}
+
+.img-header {
+    width: 200px;
+    display: block;
+    margin: auto;
+}
+
+.form-control {
+    padding: 20px 10px;
+    border-radius: 0;
+    border: none;
+    outline: none;
+    font-size: 14px;
+}
+
+.form-control:focus {
+    box-shadow: 0 0 0 transparent;
+    border: none;
+}
+
+.form-group {
+    margin-top: auto;
+}
+
+.up {
+    border-radius: 6px;
+    border-bottom: 1px solid #e4e4e4;
+}
+
+.down {
+    border-radius: 6px;
+}
+
+.up:focus {
+    border-bottom: 1px solid #e4e4e4;
+}
+
+.btn-primary {
+    background-color: #213873 !important;
+    border: 1px solid #213873 !important;
+    padding: 10px;
+    font-size: 15px;
+}
+
+@media only screen and (min-width: 600px) {
+    .container {
+        width: 500px;
+        display: block;
+        margin: 80px auto;
+    }
+}
